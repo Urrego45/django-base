625fc66722ece4e13dfa6279b79d22a1afa8ab55
adhaliel <santiagourregou.4@gmail.com>
2024-09-09 20:34:08 -0500

    ajustes

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+    'corsheaders',
+    'rest_framework',
+    'django_extensions',
+    'django_filters',
+]
+
+LOCAL_APPS = []
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
